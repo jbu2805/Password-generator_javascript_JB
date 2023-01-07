20eb280d664808b2c34a48e92eb0747659597790
jbu2805@gmail.com <jbu2805@gmail.com>
2023-01-07 23:58:06 +0000

    final version of the code that includes prompts and working password generator

--- script.js
+++ script.js
@@ -93,6 +93,8 @@ var lowercase;
 var uppercase;
 var special;
 var numeric;
+
+
 //Generate a password when the button is clicked
 //Present a series of prompts for password criteria
 //Length of password At least 10 characters but no more than 64.
@@ -103,71 +105,89 @@ var numeric;
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  length = prompt("Enter a length");
-  lowercase = confirm("Include lower case?");
-  uppercase = confirm("Include upper case?");
-  special = confirm("Include special character?");
-  numeric = confirm("Include numbers?")
-}
+
+  length = prompt("Enter a password length");
+  if (length <10 || length > 64)
+  alert("Length pf the password must be between 10 and 64")
+
+  else{
+    lowercase = confirm("Do you want to include lower case characters?");
+    // console.log(lowercasecharacter);
+    uppercase = confirm("Do you want to include upper case characters?");
+    special = confirm("Do you want to include special characters?");
+    numeric = confirm("Do you want to include numbers?");
+  }
+  }
+  
+getPasswordOptions();
 
 // Function for getting a random element from an array
 // Function for getting a random element from an array
 function getRandom(arr) {
   //get random index value
-  var randomIndex=Math.floor(Math.random()*arr.length);
-  return randomIndex;
-  }
+  return Math.floor(Math.random() * ((arr.length-1) - 1 + 1)) + 1;
+}
 
-  //concatenate all arrays in one
+
+//concatenate all arrays in one
 var array = specialCharacters.concat(numericCharacters, lowerCasedCharacters, upperCasedCharacters);
 //view all characters that could be used to create a password in an array
 console.log(array);
 //call function
-getRandom(array); 
- 
-  
-  //create variable to store randomly selected character from the array
-  result = getRandom(array);
-  entity1=getRandom(specialCharacters);
-  entity2=getRandom(numericCharacters);
-  entity3=getRandom(lowerCasedCharacters);
-  entity4=getRandom(upperCasedCharacters);
-  console.log(result);
-  console.log(entity1);  
-
+getRandom(array);
 
 // Function to generate password with user input
-// Function to generate password with user input
 
-function password_generator( len) {
-  
-  var password = "";
-  var character = "";
-
-  while( password.length<length ) {
-      hold = specialCharacters[entity1];
-      hold = upperCasedCharacters[entity4];
-      character += hold;
-      character += numericCharacters[entity2];
-      character += lowerCasedCharacters[entity3];
-      password = character;
+function generatePassword(length) {
+  var password = ""
+  if(lowercase == true || uppercase == true || special == true || numeric == true)
+  {
+    for(var i = 0; i < length;)
+    {
+      var typeChoice = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+      if (typeChoice == 1 && lowercase == true)
+      {
+        password += lowerCasedCharacters[getRandom(lowerCasedCharacters)]
+        i++
+      }
+      if (typeChoice == 2 && uppercase == true)
+      {
+        password += upperCasedCharacters[getRandom(upperCasedCharacters)]
+      i++
+      }
+      if (typeChoice == 3 && special == true)
+      {
+        password += specialCharacters[getRandom(specialCharacters)]
+      i++
+      }
+      if (typeChoice == 4 && numeric == true)
+      {
+        password += numericCharacters[getRandom(numericCharacters)]
+       i++
+      }
+    }
+    return password;
   }
-  password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-  return password.substr(0,len);
+  else{
+    alert("At least one option must be selected")
+  }
+
 }
-
-console.log( password_generator() );
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
-function writePassword() {
-  password = password_generator(length);
-  passwordText = document.querySelector('#password');
+function writePassword()   
+{if (length <10 || length > 64)
+  
+  alert("Length of the password must be between 10 and 64");
 
+else {
+  var password = generatePassword(length);
+  var passwordText = document.querySelector('#password');
   passwordText.value = password;
+
+}
 }
 
 // Add event listener to generate button
